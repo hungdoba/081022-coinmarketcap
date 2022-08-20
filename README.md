553ea81276f8523300a921f431c7a23cfec0b24c
hungdoba <hungdoba.hdb@gmail.com>
2022-08-20 09:00:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Welcome to Coinmarketcap.jp
---
+++ src/components/UI/baseline/atoms/MainLogo.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Theme, makeStyles, useTheme } from "@material-ui/core/styles";
+import { Box, Typography } from "@material-ui/core";
+import { CopyrightRounded } from "@material-ui/icons";
+import { drawerWidth } from "../../../../common/shared/dimensions";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  logoWrapper: {
+    width: `calc(${drawerWidth}px - 48px)`, // 240 - 24*2
+    textAlign: "center",
+    display: "flex",
+    alignItems: "center",
+    "& svg": {
+      marginRight: 8,
+    },
+  },
+  landscapeIcon: {
+    fill: "url(#landscapeGradient)",
+    height: theme.spacing(4),
+    width: theme.spacing(4),
+  },
+}));
+
+const MainLogo: React.FC = () => {
+  const classes = useStyles();
+  const theme = useTheme();
+
+  return (
+    <Box className={classes.logoWrapper}>
+      <svg width="0" height="0">
+        <linearGradient id="landscapeGradient">
+          <stop offset="20%" stopColor={theme.palette.primary.main} />
+          <stop offset="80%" stopColor={theme.palette.secondary.main} />
+        </linearGradient>
+      </svg>
+      <CopyrightRounded classes={{ root: classes.landscapeIcon }} />
+      <Typography variant="h6" noWrap>
+        Coinmarketcap
+      </Typography>
+    </Box>
+  );
+};
+
+export default MainLogo;
